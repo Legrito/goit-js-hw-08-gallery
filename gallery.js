13afd984e01eb2d909547647a59d3f1b6f482182
Lesya Stulkivska <67697045+Legrito@users.noreply.github.com>
2021-03-13 22:50:12 +0200

    added right-left navigation

--- gallery.js
+++ gallery.js
@@ -39,7 +39,6 @@ const onImageClick = (evt) => {
 const closeButton = document.querySelector('button[data-action="close-lightbox"]');
 const lightboxImage = document.querySelector('img.lightbox__image');
 const onModalIsClosed = (evt) => {
-    console.log(evt.target);
      lightboxImage.src = ''; 
     lightbox.classList.remove('is-open');
      
@@ -52,15 +51,65 @@ galleryList.addEventListener('click', onImageClick);
 const onByEscButtonClose = (evt) => {
     evt.preventDefault();
     if (evt.keyCode == 27) {
-        console.log(evt.keyCode);
         lightboxImage.src = '';
         lightbox.classList.remove('is-open');
-    } else {
-        return;
     }
 }
-window.addEventListener('keydown', onByEscButtonClose)
-// window.addEventListener('keydown', (evt) => {
-//     console.log(evt.keyCode);
-//     return evt.key;
-// });
+
+window.addEventListener('keydown', onByEscButtonClose);
+
+const onSlideToLeft = (evt) => {
+    evt.preventDefault();
+    if (evt.keyCode == 37) {
+        const currentImageIndex = pictures
+            .findIndex((image) => image.original === lightboxImage.src);
+        console.log(currentImageIndex);
+        if (currentImageIndex > 0) {
+                lightboxImage.src = pictures[currentImageIndex - 1].original;
+        console.log('left');  
+        }
+  
+    }
+};
+
+const onSlideToRight = (evt) => {
+    evt.preventDefault();
+    if (evt.keyCode == 39) {
+        const currentImageIndex = pictures
+            .findIndex((image) => image.original === lightboxImage.src);
+        console.log(currentImageIndex);
+        if (currentImageIndex < pictures.length - 1) {
+            lightboxImage.src = pictures[currentImageIndex + 1].original;
+        console.log('right');
+        }        
+    } 
+};
+
+// const onSlideToLeft = (evt) => {
+//     evt.preventDefault();
+//     if (evt.keyCode === 37) {
+//         pictures.map((image, idx) => {            
+//             if (image.original === lightboxImage.src && idx !== 0) {
+//                 console.log(idx);
+//                 lightboxImage.src = pictures[idx - 1].original;
+//             }
+//         });
+//     }
+// };
+
+// const onSlideToRight = (evt) => {
+//     evt.preventDefault();
+//     if (evt.keyCode === 39) {
+//         pictures.map((image, idx) => {
+//             if (image.original === lightboxImage.src && idx !== pictures.length - 1) {
+//                 console.log(idx);
+//                 lightboxImage.src = pictures[idx + 1].original;
+//             }
+//         });
+//     }
+// };
+
+window.addEventListener('keydown', onSlideToRight);
+window.addEventListener('keydown', onSlideToLeft);
+
+//37 - Left 39 - rigth
